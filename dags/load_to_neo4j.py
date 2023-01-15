68943c99f37b53aece2a4021e2645917eab62ceb
reijosepp <reijosepp97@gmail.com>
2023-01-15 15:15:35 +0200

    load to neo4j dag improvements

--- dags/load_to_neo4j.py
+++ dags/load_to_neo4j.py
@@ -27,44 +27,44 @@ with DAG(
     driver = neo4j.GraphDatabase.driver('bolt://172.25.0.4', auth=None)
 
     def create_article_node(doi, title, pages, figures):
-        return f'CREATE (n:Article {{IdMaster: "{doi}", title: "{title}", pages: {pages}, figures: {figures}}})'
+        return f'CREATE (n:Article {{doi: "{doi}", title: "{title}", pages: {pages}, figures: {figures}}})'
 
     def create_author_node(author):
-        return f'CREATE (n:Author {{IdMaster: "{author}"}})'
+        return f'CREATE (n:Author {{name: "{author}"}})'
 
     def create_journal_node(journal):
-        return f'CREATE (n:Journal {{IdMaster: "{journal}"}})'
+        return f'CREATE (n:Journal {{journal: "{journal}"}})'
     
     def create_category_node(category):
-        return f'CREATE (n:Category {{IdMaster: "{category}"}})'
+        return f'CREATE (n:Category {{category: "{category}"}})'
 
     def create_article_version_node(doi, version, created):
-        return f'CREATE (n:Version {{IdMaster: "{doi + " - " + version}, doi: "{doi}", version: "{version}", created: "{created}""}})'
+        return f'CREATE (n:Version {{doi: "{doi}", version: "{version}", created: "{created}""}})'
 
     def create_year_node(year):
-        return f'CREATE (n:Year {{IdMaster: "{year}"}})'
+        return f'CREATE (n:Year {{year: "{year}"}})'
 
     def create_authorship_relationship(doi, author):
         return (f'MATCH (a:Author), (b:Article)'
-                f'WHERE a.IdMaster = "{author}" AND b.IdMaster = "{doi}"' 
+                f'WHERE a.name = "{author}" AND b.doi = "{doi}"' 
                 f'CREATE (a)-[r:AUTHORS]->(b)')
 
     def create_is_published_in_relationship(doi, journal):
         return (f'MATCH (a:Article), (b:Journal)' 
-                f'WHERE a.IdMaster = "{doi}" AND b.IdMaster = "{journal}"' 
+                f'WHERE a.doi = "{doi}" AND b.journal = "{journal}"' 
                 f'CREATE (a)-[r:IS_PUBLISHED_IN]->(b)'	)
 
     def create_belongs_to_relationship(doi, category):
         return (f'MATCH (a:Article), (b:Category)' 
-                f'WHERE a.IdMaster = "{doi}" AND b.IdMaster = "{category}"' 
+                f'WHERE a.doi = "{doi}" AND b.category = "{category}"' 
                 f'CREATE (a)-[r:BELONGS_TO]->(b)')
 
-    def node_exists(type, id):
-        return (f'OPTIONAL MATCH (n:{type} {{IdMaster: "{id}" }})'
+    def node_exists(type, field, id):
+        return (f'OPTIONAL MATCH (n:{type} {{{field}: "{id}" }})'
                 f'RETURN n IS NOT NULL AS Predicate')
     
-    def relationship_exists(type_r, type_a, type_b, id_a, id_b):
-        return(f'OPTIONAL MATCH (a:{type_a} {{IdMaster: "{id_a}"}})-[r:{type_r}]->(b:{type_b} {{IdMaster: "{id_b}"}})'
+    def relationship_exists(type_r, type_a, type_b, field_a, field_b, id_a, id_b):
+        return(f'OPTIONAL MATCH (a:{type_a} {{{field_a}: "{id_a}"}})-[r:{type_r}]->(b:{type_b} {{{field_b}: "{id_b}"}})'
                f'RETURN r IS NOT NULL AS Predicate')
 
     ############# Pipeline Functions #############
@@ -93,25 +93,25 @@ with DAG(
 
                     row = data.iloc[i, :]
 
-                    if (not session.run(node_exists('Article', row['doi'])).single().value()):
+                    if (not session.run(node_exists('Article', 'doi', row['doi'])).single().value()):
                         session.run(create_article_node(row['doi'], row['title'].replace('"', '').replace('\\', ''), row['pages'], row['figures']))
                     
-                    if (not session.run(node_exists('Author', row['author'])).single().value()):
+                    if (not session.run(node_exists('Author', 'author', row['author'])).single().value()):
                         session.run(create_author_node(row['author']))
                     
-                    if (not session.run(node_exists('Journal', str(row['journal-ref']).replace('"', ''))).single().value()):
+                    if (not session.run(node_exists('Journal', 'journal', str(row['journal-ref']).replace('"', ''))).single().value()):
                         session.run(create_journal_node(str(row['journal-ref']).replace('"', '')))
                     
-                    if (not session.run(node_exists('Category', row['categories'])).single().value()):
+                    if (not session.run(node_exists('Category', 'category', row['categories'])).single().value()):
                         session.run(create_category_node(row['categories']))
                     
-                    if (not session.run(relationship_exists('AUTHORS', 'Author', 'Article', row['author'], row['doi'])).single().value()):
+                    if (not session.run(relationship_exists('AUTHORS', 'Author', 'Article', 'name', 'doi', row['author'], row['doi'])).single().value()):
                         session.run(create_authorship_relationship(row['doi'], row['author']))
 
-                    if (not session.run(relationship_exists('IS_PUBLISHED_IN', 'Article', 'Journal', row['doi'], str(row['journal-ref']).replace('"', ''))).single().value()):
+                    if (not session.run(relationship_exists('IS_PUBLISHED_IN', 'Article', 'Journal', 'doi', 'journal', row['doi'], str(row['journal-ref']).replace('"', ''))).single().value()):
                         session.run(create_is_published_in_relationship(row['doi'], str(row['journal-ref']).replace('"', '')))
 
-                    if (not session.run(relationship_exists('BELONGS_TO', 'Article', 'Category', row['doi'], row['categories'])).single().value()):
+                    if (not session.run(relationship_exists('BELONGS_TO', 'Article', 'Category', 'doi', 'category', row['doi'], row['categories'])).single().value()):
                         session.run(create_belongs_to_relationship(row['doi'], row['categories']))
 
             driver.close()
